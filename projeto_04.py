f211326605ae63177787d1d04edf7a1386b585c6
hygorrasec <hygorrasec@gmail.com>
2023-03-30 00:00:21 -0300

    first commit

---
+++ projeto_04.py
@@ -0,0 +1,16 @@
+# UNIVERSIDADE DE VASSOURAS - MARICÁ
+# PROFESSOR: MÁRCIO GARRIDO
+# ESTUDANTES: FÁBIO LIMA, HYGOR RASEC, VICTOR BERNARDO
+# 30/03/2023
+# PROJETO 04
+
+# Tendo em vista o conjunto de elementos abaixo, criar um algoritmo que seja capaz de determinar a posição de cada elemento e printar as de forma ascendente os valores, assim como printar separadamente posições atuais. N ofinal imprimir de forma gráfica a notação Big'O do seu resultado.
+
+# // Entrada
+
+# elementos = [[a,b,c,d],[q,i,n,m],[f,e,h,j],[p,o,l,g]]
+
+# // Saída
+
+# [a,b,c,d,e,f,g,h,i,j,l,m,n,o,p,q]
+# [0:0,0:1,0:2,0:3,2:1,2:0,3:3,2:2,1:1,2:3,3:2,1:3,1:2,3:1,3:0,1:0]
